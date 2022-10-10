f9460f8cb64caa83e71b9dce327a06ff85ceb489
Shivan <22425471+Laurent-Nicolini@users.noreply.github.com>
2022-10-10 15:43:55 +0200

    Gestion du role dans login

--- connexion.php
+++ connexion.php
@@ -7,16 +7,15 @@ if(isset($_POST['email']) && isset($_POST['password']))
     // connexion à la base de données
     try{
         $pdo = new PDO("mysql:host=localhost;dbname=appwebsport","root","");
-        $statement = $pdo->prepare("SELECT * FROM users WHERE email =:email AND password =:password");
+        $statement = $pdo->prepare("SELECT * FROM users WHERE email =:email AND password =:password AND role =:role");
         $statement->bindValue(':email', $email, PDO::PARAM_STR);
         $statement->bindValue(':password', $password, PDO::PARAM_STR);
+        $statement->bindValue(':role', $role, PDO::PARAM_BOOL);
     }
      catch(PDOException $e){
         echo $e->getMessage();
      }
-    
-    // on applique les deux fonctions mysqli_real_escape_string et htmlspecialchars
-    // pour éliminer toute attaque de type injection SQL et XSS
+
     $statement->execute();
     $result = $statement->fetchAll();
     var_dump($result);
