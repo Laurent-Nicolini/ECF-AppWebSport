14e4a483f15e9800b91df89d2de20e9e7ca7f4c4
Shivan <22425471+Laurent-Nicolini@users.noreply.github.com>
2022-10-13 22:17:22 +0200

    Affichage Deconnexion dans Menu

--- menu.php
+++ menu.php
@@ -1,13 +1,15 @@
 <nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">Admin App Web Sport</a>
-
+            <div class="d-flex flex-column" >
+                <a class="navbar-brand" href="index.php">Admin App Web Sport</a>
+                <p class=""><a href='deconnexion.php'>Déconnexion</a></p>
+            </div>
             <div class="container-fluid d-flex">
                 <ul class="navbar-nav align-items-center">
                     <li class="nav-item active">
                         <a class="nav-link" href="allusers.php">Voir tous les partenaires</a>
                     </li>
-                    <li class="nav-item me-sm-3">
+                    <li class="nav-item ">
                         <a class="nav-link" href="addpart.php">Ajouter un partenaire</a>
                     </li>            
                 </ul>
@@ -18,5 +20,3 @@
             </div>
         </div>
     </nav>
-<p class="d-flex flex-row-reverse me-2"><a href='deconnexion.php'>Déconnexion</a></p>
-<br>
