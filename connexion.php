dd83b85a2b6246acaf5c00f3e24262eb82aa7038
Shivan <22425471+Laurent-Nicolini@users.noreply.github.com>
2022-10-19 21:50:04 +0200

    Supp code en com + Redirection erreur mdp

--- connexion.php
+++ connexion.php
@@ -37,38 +37,10 @@ if(isset($_POST['email']) && isset($_POST['password']))
                header('Location: login.php?erreur=1'); // utilisateur ou mot de passe incorrect
             }
          }
+   } else {
+      header('Location: login.php?erreur=1');
+      exit();
    }
-    /*try{
-        $pdo = new PDO("mysql:host=localhost;dbname=appwebsport","root","");
-        $statement2 = $pdo->prepare("SELECT * FROM users WHERE email =:email AND password =:password");
-        $statement2->bindValue(':email', $email, PDO::PARAM_STR);
-        $statement2->bindValue(':password', $password, PDO::PARAM_STR);
-    }
-     catch(PDOException $e){
-        echo $e->getMessage();
-     }
-    $statement2->execute();
-    $result2 = $statement2->fetchAll(PDO::FETCH_OBJ);
-    if($email=$result && $password=$result){
-    foreach($result2 as $results2){
-      
-         if($results2->Role == 1){
-           $_SESSION['role'] = 1;
-           header('Location: index.php'); 
-         } elseif ($results2->Role == 0) {
-            $_SESSION['role'] = 0;
-            $_SESSION['usersid'] = intval($results2->Id);
-            $usersid = intval($results2->Id);
-            header("Location: partenaire.php?users_id=$usersid");
-         } else
-            {
-               header('Location: login.php?erreur=1'); // utilisateur ou mot de passe incorrect
-            }
-         }
-      } else {
-         header('Location: login.php?erreur=1');
-         exit();
-      }*/
 } else {
    header('Location: login.php?erreur=1');
    exit();
