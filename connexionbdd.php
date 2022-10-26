299e13d15fb3075d5c3bebc538a5dc63673d3ce3
Shivan <22425471+Laurent-Nicolini@users.noreply.github.com>
2022-10-26 21:34:48 +0200

    Ajout utilisateur inactif, connexion impossible

--- connexionbdd.php
+++ connexionbdd.php
@@ -1,6 +1,7 @@
 <?php
    try{
-      $pdo = new PDO("mysql:host=localhost;dbname=odrl9643_appwebsport","odrl9643_awsadmin","App2022+");
+      //$pdo = new PDO("mysql:host=localhost;dbname=odrl9643_appwebsport","odrl9643_awsadmin","App2022+");
+      $pdo = new PDO("mysql:host=localhost;dbname=appwebsport","root","");
       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
    }
    catch(PDOException $e){
