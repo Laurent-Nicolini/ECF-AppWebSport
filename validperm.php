cc6e26397f4fc4a1f62d0ddf2ef428b36affcde2
Shivan <22425471+Laurent-Nicolini@users.noreply.github.com>
2022-10-27 21:29:34 +0200

    MAJ bug comparaison entre int et string

--- validperm.php
+++ validperm.php
@@ -3,55 +3,67 @@ session_start();
 $idstructure = $_GET['idstructure'];
 $partenaire = $_GET['users_id'];
 
+if(!isset($_POST['boissons'])) { $boissons = 0;} else { $boissons = 1;}
+if(!isset($_POST['planning'])) { $planning = 0;} else { $planning = 1;}
+if(!isset($_POST['newsletter'])) { $newsletter = 0;} else { $newsletter = 1;}
+
+var_dump($boissons,$planning,$newsletter);
+//exit();
+
 include_once 'connexionbdd.php'; 
 
 $stt = $pdo->prepare("SELECT perm_boissons,perm_planning,perm_newsletter FROM structure WHERE id=:idstructure");
-$stt->bindValue(":idstructure", $_GET['idstructure'],PDO::PARAM_INT);
+$stt->bindValue(":idstructure", $idstructure,PDO::PARAM_INT);
 $stt->execute();
 $requete = $stt->fetchAll(PDO::FETCH_OBJ);
 foreach($requete as $requetes){
+    var_dump($requetes);
+    //exit();
 
-    if ($_POST['boissons'] != $requetes->perm_boissons){
+    if ($boissons != intval($requetes->perm_boissons)){
+        var_dump(intval($requetes->perm_boissons));
+        //exit();
         if ($requetes->perm_boissons == 0){
-        $statement1 = $pdo->prepare("UPDATE structure SET perm_boissons = 1 WHERE Id = :idstructure");
-        $statement1->bindValue(":idstructure", $_GET['idstructure'], PDO::PARAM_INT);
-        $statement1->execute();
-        $req1 = $statement1->fetch(PDO::FETCH_OBJ);
+        $statement = $pdo->prepare("UPDATE structure SET perm_boissons = 1 WHERE Id = :idstructure");
+        $statement->bindValue(":idstructure", $idstructure, PDO::PARAM_INT);
+        $statement->execute();
+        $req = $statement->fetch(PDO::FETCH_OBJ);
     } else {
-        $statement2 = $pdo->prepare("UPDATE structure SET perm_boissons = 0 WHERE Id = :idstructure");
-        $statement2->bindValue(":idstructure", $_GET['idstructure'], PDO::PARAM_INT);
-        $statement2->execute();
-        $req2 = $statement2->fetch(PDO::FETCH_OBJ);
+        $statement = $pdo->prepare("UPDATE structure SET perm_boissons = 0 WHERE Id = :idstructure");
+        $statement->bindValue(":idstructure", $idstructure, PDO::PARAM_INT);
+        $statement->execute();
+        $req = $statement->fetch(PDO::FETCH_OBJ);
     }
-    }
+    }   
 
-    if ($_POST['planning'] != $requetes->perm_planning){
+    if ($planning != intval($requetes->perm_planning)){
         if ($requetes->perm_planning == 0){
-        $statement3 = $pdo->prepare("UPDATE structure SET perm_planning = 1 WHERE Id = :idstructure");
-        $statement3->bindValue(":idstructure", $_GET['idstructure'], PDO::PARAM_INT);
-        $statement3->execute();
-        $req3 = $statement3->fetch(PDO::FETCH_OBJ);
+        $statement = $pdo->prepare("UPDATE structure SET perm_planning = 1 WHERE Id = :idstructure");
+        $statement->bindValue(":idstructure", $idstructure, PDO::PARAM_INT);
+        $statement->execute();
+        $req = $statement->fetch(PDO::FETCH_OBJ);
     } else {
-        $statement4 = $pdo->prepare("UPDATE structure SET perm_planning = 0 WHERE Id = :idstructure");
-        $statement4->bindValue(":idstructure", $_GET['idstructure'], PDO::PARAM_INT);
-        $statement4->execute();
-        $req4 = $statement4->fetch(PDO::FETCH_OBJ);
+        $statement = $pdo->prepare("UPDATE structure SET perm_planning = 0 WHERE Id = :idstructure");
+        $statement->bindValue(":idstructure", $idstructure, PDO::PARAM_INT);
+        $statement->execute();
+        $req = $statement->fetch(PDO::FETCH_OBJ);
     }
     }
 
-    if ($_POST['newsletter'] != $requetes->perm_newsletter){
+    if ($newsletter != intval($requetes->perm_newsletter)){
         if ($requetes->perm_newsletter == 0){
-        $statement5 = $pdo->prepare("UPDATE structure SET perm_newsletter = 1 WHERE Id = :idstructure");
-        $statement5->bindValue(":idstructure", $_GET['idstructure'], PDO::PARAM_INT);
-        $statement5->execute();
-        $req5 = $statement5->fetch(PDO::FETCH_OBJ);
+        $statement = $pdo->prepare("UPDATE structure SET perm_newsletter = 1 WHERE Id = :idstructure");
+        $statement->bindValue(":idstructure", $idstructure, PDO::PARAM_INT);
+        $statement->execute();
+        $req = $statement->fetch(PDO::FETCH_OBJ);
     } else {
-        $statement6 = $pdo->prepare("UPDATE structure SET perm_newsletter = 0 WHERE Id = :idstructure");
-        $statement6->bindValue(":idstructure", $_GET['idstructure'], PDO::PARAM_INT);
-        $statement6->execute();
-        $req6 = $statement6->fetch(PDO::FETCH_OBJ);
+        $statement = $pdo->prepare("UPDATE structure SET perm_newsletter = 0 WHERE Id = :idstructure");
+        $statement->bindValue(":idstructure", $idstructure, PDO::PARAM_INT);
+        $statement->execute();
+        $req = $statement->fetch(PDO::FETCH_OBJ);
     }
     }
+
     $_SESSION['permvalid'] = 1;
     header("Location: structure.php?idstructure=$idstructure&users_id=$partenaire");
     exit();
