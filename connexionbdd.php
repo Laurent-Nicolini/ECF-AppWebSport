9ca09ae8341a7174f7fda4c4ae756dd0576ee285
Shivan <22425471+Laurent-Nicolini@users.noreply.github.com>
2023-01-13 08:33:16 +0100

    Delete connexionbdd.php

--- connexionbdd.php
+++
@@ -1,9 +0,0 @@
-<?php
-   try{
-      $pdo = new PDO("mysql:host=localhost;dbname=odrl9643_appwebsport","odrl9643_awsadmin","App2022+");
-      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   }
-   catch(PDOException $e){
-      exit('<b>Catched exception at line '. $e->getLine() .' :</b> '. $e->getMessage());
-   }
-?>
